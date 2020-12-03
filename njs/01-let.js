350ec357761f5e99f6a73b3cf2eb5e7d54b28d67
DESKTOP-QSMSQE0\diennk <trietnk01@gmail.com>
2020-12-04 00:05:52 +0700

    es6 excersise

---
+++ njs/01-let.js
@@ -0,0 +1,33 @@
+//========VAR==========
+/*var score = 100;
+if(score > 50){
+    var msg="WIN";
+    console.log(msg);
+}
+console.log(score);
+console.log(msg);*/
+//=========LET===========
+/*let score = 100;
+let msg="";
+if(score > 50){
+    let msg="WIN";
+    console.log(msg);
+}
+console.log(score);
+console.log(msg);*/
+
+/*function drawShape(){
+    var mContent= document.getElementById("mContent");
+    for(let i=1 ; i<=18;i++){
+        var elmP=document.createElement("p");
+        elmP.innerHTML=i;
+        elmP.onclick=function(){
+            alert("Selected: #" + i);
+        }
+        mContent.appendChild(elmP);
+    }
+}
+function clearShape(){
+    var mContent= document.getElementById("mContent");
+    mContent.innerHTML="";
+}*/
